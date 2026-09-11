3cb1008dafc4451e82c292963e27603846eea197
henriqux77 <duartehenrique245@gmail.com>
2026-09-10 21:16:30 -0300

    chore: remove obsolete legacy theme renderer

--- js/core/theme-caverna.js
+++
@@ -1,8 +0,0 @@
-/*
- * Legacy compatibility module.
- * Campaign themes are owned by js/core/theme-ui.js + js/core/theme.js.
- * This file intentionally does not render or mutate the theme selector.
- */
-(() => {
-  "use strict";
-})();
