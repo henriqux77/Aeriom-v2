20ab0789eddea58e0c39134d2c339c2069c6e1c6
henriqux77 <duartehenrique245@gmail.com>
2026-09-12 16:11:46 -0300

    Finalize master dashboard visual layer

--- js/core/theme-ui.js
+++ js/core/theme-ui.js
@@ -3,6 +3,7 @@ import "./campaign-ui-repair.js?v=20260912-master-shell2";
 import "./campaign-live-media.js?v=20260912-live2";
 import "./mestre-controles-compact.js?v=20260912-master-redesign1";
 import "./mestre-dashboard-redesign.js?v=20260912-master2";
+import "./mestre-dashboard-redesign-patch.js?v=20260912-master2";
 
 (() => {
   "use strict";
