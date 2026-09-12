aacec3f8cfb3912732be6beacc46ce87c1168ecd
henriqux77 <duartehenrique245@gmail.com>
2026-09-12 14:43:17 -0300

    Bump theme entrypoint cache version

--- js/core/theme-ui.js
+++ js/core/theme-ui.js
@@ -1,8 +1,8 @@
 /* AERIOM theme entrypoint. Atmosfera owns the theme selector UI. */
-import "./campaign-ui-repair.js?v=20260912-master-shell1";
+import "./campaign-ui-repair.js?v=20260912-master-shell2";
 import "./mestre-controles-compact.js?v=20260912-master-redesign1";
 
 (() => {
   "use strict";
-  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-12-master-redesign1" });
+  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-12-master-shell2" });
 })();
