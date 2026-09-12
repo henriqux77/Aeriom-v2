2db4b6c2ff74e56cc5846a14120e5e05f646046e
henriqux77 <duartehenrique245@gmail.com>
2026-09-12 16:11:23 -0300

    Load master dashboard redesign and live media UI

--- js/core/theme-ui.js
+++ js/core/theme-ui.js
@@ -1,8 +1,10 @@
-/* AERIOM theme entrypoint. Atmosfera owns the theme selector UI. */
+/* AERIOM theme entrypoint. Atmosfera and Master Dashboard are layered UI modules. */
 import "./campaign-ui-repair.js?v=20260912-master-shell2";
+import "./campaign-live-media.js?v=20260912-live2";
 import "./mestre-controles-compact.js?v=20260912-master-redesign1";
+import "./mestre-dashboard-redesign.js?v=20260912-master2";
 
 (() => {
   "use strict";
-  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-12-master-shell2" });
+  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-12-master-dashboard2" });
 })();
