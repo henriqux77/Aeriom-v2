bb200bc17a46c7a9c7fbc1422a89206f03343674
henriqux77 <duartehenrique245@gmail.com>
2026-09-13 17:44:51 -0300

    Fix character builder shared profile bootstrap mode

--- afterlife/js/ficha-criacao-v9.js
+++ afterlife/js/ficha-criacao-v9.js
@@ -1,2 +1,5 @@
-import './profile-global.js';
-import 'https://raw.githubusercontent.com/henriqux77/Aeriom-v2/d1c402af02bb9b3955c5f8974304ee5b10b7b1be/afterlife/js/ficha-criacao-v9.js';
+(() => {
+  'use strict';
+  const load=(src)=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.defer=true;s.onload=resolve;s.onerror=reject;document.head.appendChild(s);});
+  load('./js/profile-global.js').then(()=>load('https://raw.githubusercontent.com/henriqux77/Aeriom-v2/d1c402af02bb9b3955c5f8974304ee5b10b7b1be/afterlife/js/ficha-criacao-v9.js')).catch(console.error);
+})();
