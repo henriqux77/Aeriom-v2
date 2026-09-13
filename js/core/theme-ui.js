a651272bc717bec717e3fd4c8f193fcd56523549
henriqux77 <duartehenrique245@gmail.com>
2026-09-12 22:48:39 -0300

    Fix campaign overflow menu integration

--- js/core/theme-ui.js
+++ js/core/theme-ui.js
@@ -4,10 +4,10 @@
 import "./campaign-interaction-repair.js?v=20260912-interaction1";
 import "./campaign-feature-loader.js?v=20260912-features1";
 import "./campaign-ui-polish.js?v=20260912-ui-polish2";
-import "./campaign-legacy-navigation-kill.js?v=20260913-legacy-nav2";
+import "./campaign-legacy-navigation-kill-v3.js?v=20260913-legacy-nav3";
 import "./campaign-player-leave.js?v=20260913-player-leave1";
 
 (() => {
   "use strict";
-  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-13-safe-lazy5" });
+  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-13-safe-lazy6" });
 })();
