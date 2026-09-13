c15ee63a327a3ccda5d86c1c00c68247576d8208
henriqux77 <duartehenrique245@gmail.com>
2026-09-12 21:50:13 -0300

    Load permanent cleanup for legacy campaign navigation

--- js/core/theme-ui.js
+++ js/core/theme-ui.js
@@ -4,8 +4,9 @@
 import "./campaign-interaction-repair.js?v=20260912-interaction1";
 import "./campaign-feature-loader.js?v=20260912-features1";
 import "./campaign-ui-polish.js?v=20260912-ui-polish2";
+import "./campaign-legacy-navigation-kill.js?v=20260913-legacy-nav1";
 
 (() => {
   "use strict";
-  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-12-safe-lazy2" });
+  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-13-safe-lazy3" });
 })();
