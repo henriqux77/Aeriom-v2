100562bfe997d9a524e876af5f23f169557a7cfc
henriqux77 <duartehenrique245@gmail.com>
2026-09-12 22:30:00 -0300

    Wire player campaign exit and cleanup

--- js/core/theme-ui.js
+++ js/core/theme-ui.js
@@ -5,8 +5,9 @@ import "./campaign-interaction-repair.js?v=20260912-interaction1";
 import "./campaign-feature-loader.js?v=20260912-features1";
 import "./campaign-ui-polish.js?v=20260912-ui-polish2";
 import "./campaign-legacy-navigation-kill.js?v=20260913-legacy-nav2";
+import "./campaign-player-leave.js?v=20260913-player-leave1";
 
 (() => {
   "use strict";
-  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-13-safe-lazy4" });
+  window.AERIOM_THEME_UI = Object.freeze({ version: "2026-09-13-safe-lazy5" });
 })();
