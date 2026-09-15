d24f053fc1a49bc4353ab7cef78d72a54767456b
henriqux77 <duartehenrique245@gmail.com>
2026-09-15 12:38:34 -0300

    bump sidebar auth client cache version

--- afterlife/js/afterlife-sidebar.js
+++ afterlife/js/afterlife-sidebar.js
@@ -1,6 +1,6 @@
-import './error-monitor.js?v=20260915-5';
-import './aeriom-client.js?v=20260915-5';
-import './shared-profile.js?v=20260915-5';
+import './error-monitor.js?v=20260915-6';
+import './aeriom-client.js?v=20260915-6';
+import './shared-profile.js?v=20260915-6';
 
 (() => {
   'use strict';
