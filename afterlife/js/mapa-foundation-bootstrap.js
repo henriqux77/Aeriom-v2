6a51ba66e9687907ff9d5dc710436b3cef2f76d8
henriqux77 <duartehenrique245@gmail.com>
2026-09-15 23:20:38 -0300

    Route campaign location selection to standalone picker

--- afterlife/js/mapa-foundation-bootstrap.js
+++ afterlife/js/mapa-foundation-bootstrap.js
@@ -4,15 +4,12 @@
   const qs = new URLSearchParams(location.search);
   const picker = qs.get('select') === '1';
 
-  const load = async () => {
-    if (picker) {
-      await import('./mapa-selector.js?v=20260916-1');
-      return;
-    }
-    await import('./mapa-foundation.js?v=20260915-6');
-  };
+  if (picker) {
+    location.replace('./campanha-local.html?return=create');
+    return;
+  }
 
-  load().catch((error) => {
+  import('./mapa-foundation.js?v=20260915-6').catch((error) => {
     console.error('[AFTERLIFE][MAP][BOOTSTRAP]', error);
     const status = document.getElementById('mapStatus');
     if (status) {
