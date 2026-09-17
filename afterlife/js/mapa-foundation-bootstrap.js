8bd4fa3a53b8656a30e03b6ba6524710d3f0d9df
henriqux77 <duartehenrique245@gmail.com>
2026-09-17 00:27:47 -0300

    chore(afterlife): bump P5.5 map area module cache

--- afterlife/js/mapa-foundation-bootstrap.js
+++ afterlife/js/mapa-foundation-bootstrap.js
@@ -10,7 +10,7 @@
   }
 
   import('./mapa-foundation.js?v=20260916-2')
-    .then(() => import('./mapa-location-areas.js?v=20260916-1'))
+    .then(() => import('./mapa-location-areas.js?v=20260917-2'))
     .catch((error) => {
       console.error('[AFTERLIFE][MAP][BOOTSTRAP]', error);
       const status = document.getElementById('mapStatus');
